af3e4d3e33a458fb88debd300083f41d467615bd
J_ManG <kjamong2020@gmail.com>
2025-07-15 11:35:16 +0900

    Edit: no-var: error to warning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn", // 에러를 경고로 변경
+      "@typescript-eslint/no-explicit-any": "warn", // 에러를 경고로 변경
+    },
+  },
 ];
 
 export default eslintConfig;
